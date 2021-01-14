937754d67709f1233b33e5e6a0b9efd383b67f0c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-14 10:27:31 +0700

    Integrated the view binding

--- app/src/main/java/io/erikrios/github/myviewmodel/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/myviewmodel/MainActivity.kt
@@ -1,11 +1,25 @@
 package io.erikrios.github.myviewmodel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.erikrios.github.myviewmodel.adapters.WeatherAdapter
+import io.erikrios.github.myviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: WeatherAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = WeatherAdapter()
+        adapter.notifyDataSetChanged()
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
     }
 }
