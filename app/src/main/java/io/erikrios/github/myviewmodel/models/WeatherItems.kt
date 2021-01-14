e3896286f88a40f0160aef44b7ce14529b59c7dc
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-14 10:16:45 +0700

    Create WeatherItems.kt

---
+++ app/src/main/java/io/erikrios/github/myviewmodel/models/WeatherItems.kt
@@ -0,0 +1,9 @@
+package io.erikrios.github.myviewmodel.models
+
+class WeatherItems {
+    var id: Int = 0
+    var name: String? = null
+    var currentWeather: String? = null
+    var description: String? = null
+    var temprerature: String? = null
+}
