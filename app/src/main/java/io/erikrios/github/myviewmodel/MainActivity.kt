e4b369cb339033663f55d5f5b46d9fd3bdfa7619
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-14 11:06:18 +0700

    Integrated the view model

--- app/src/main/java/io/erikrios/github/myviewmodel/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/myviewmodel/MainActivity.kt
@@ -2,23 +2,19 @@ package io.erikrios.github.myviewmodel
 
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.loopj.android.http.AsyncHttpClient
-import com.loopj.android.http.AsyncHttpResponseHandler
-import cz.msebera.android.httpclient.Header
 import io.erikrios.github.myviewmodel.adapters.WeatherAdapter
 import io.erikrios.github.myviewmodel.databinding.ActivityMainBinding
-import io.erikrios.github.myviewmodel.models.WeatherItems
-import org.json.JSONObject
-import java.text.DecimalFormat
+import io.erikrios.github.myviewmodel.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: WeatherAdapter
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +27,22 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
 
+        mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+
         binding.btnCity.setOnClickListener {
             val city = binding.editCity.text.toString()
             if (city.isEmpty()) return@setOnClickListener
             showLoading(true)
-            setWeather(city)
+
+            mainViewModel.setWeather(city)
         }
+
+        mainViewModel.getWeathers().observe(this, { weatherItems ->
+            weatherItems?.let {
+                adapter.setData(weatherItems)
+                showLoading(false)
+            }
+        })
     }
 
     private fun showLoading(state: Boolean) {
@@ -45,45 +51,5 @@ class MainActivity : AppCompatActivity() {
         } else {
             binding.progressBar.visibility = View.GONE
         }
-    }
-
-    private fun setWeather(cities: String) {
-        val listItems = ArrayList<WeatherItems>()
-
-        val apiKey = "c55d4a58da4dc4d141c1adbcb64cd509"
-        val url = "https://api.openweathermap.org/data/2.5/group?id=${cities}&units=metric&appid=${apiKey}"
-
-        val client = AsyncHttpClient()
-        client.get(url, object : AsyncHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?) {
-                try {
-                    val result = String(responseBody as ByteArray)
-                    val responseObject = JSONObject(result)
-                    val list = responseObject.getJSONArray("list")
-
-                    for (i in 0 until list.length()) {
-                        val weather = list.getJSONObject(i)
-                        val weatherItems = WeatherItems()
-                        weatherItems.id = weather.getInt("id")
-                        weatherItems.name = weather.getString("name")
-                        weatherItems.currentWeather = weather.getJSONArray("weather").getJSONObject(0).getString("main")
-                        weatherItems.description = weather.getJSONArray("weather").getJSONObject(0).getString("description")
-                        val tempInKelvie = weather.getJSONObject("main").getDouble("temp")
-                        val tempInCelcius = tempInKelvie - 273
-                        weatherItems.temprerature = DecimalFormat("##.##").format(tempInCelcius)
-                        listItems.add(weatherItems)
-                    }
-
-                    adapter.setData(listItems)
-                    showLoading(false)
-                } catch (e: Exception) {
-                    Log.d("Exception", e.message.toString())
-                }
-            }
-
-            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
-                Log.d("onFailure", error?.message.toString())
-            }
-        })
     }
 }
